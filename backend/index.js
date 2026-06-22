74f45d4e3a1e00db9fb830af58aca8d2e7f3eb39
Ritik <ritikrajput543678@gmail.com>
2026-06-22 17:55:25 +0530

    fix cors

--- backend/index.js
+++ backend/index.js
@@ -9,19 +9,22 @@ import userRouter from "./routes/user.routes.js"
 import geminiResponse from "./gemini.js"
 
 
-const app=express()
+const app = express()
 app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
+    origin: [
+        "http://localhost:5173",
+        "https://virtual-assistant-gamma-rust.vercel.app"
+    ],
+    credentials: true
 }))
-const port=process.env.PORT || 5000
+const port = process.env.PORT || 5000
 app.use(express.json())
 app.use(cookieParser())
-app.use("/api/auth",authRouter)
-app.use("/api/user",userRouter)
+app.use("/api/auth", authRouter)
+app.use("/api/user", userRouter)
 
 
-app.listen(port,()=>{
+app.listen(port, () => {
     connectDb()
     console.log("server started")
 })
